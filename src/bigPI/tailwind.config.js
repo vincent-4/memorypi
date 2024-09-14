a297e4da5433fc5f608019ce61c8ce2caf838aa2
matchtae <williamchatea@gmail.com>
2024-09-14 19:47:21 -0400

    Init flask. > > Co-authored-by: Encryptex jaume@hackupc.com

---
+++ src/bigPI/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/**/*.html',
+    './static/**/*.js',
+    './node_modules/flowbite/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
+
